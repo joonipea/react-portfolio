17b3d53c81b917e574490317fd856043cbaf04ba
June Pratt <juneprattm@gmail.com>
2022-05-16 23:05:44 -0400

    hide email from crawlers in about section

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -13,7 +13,23 @@ function About() {
         }
         return age;
     }
+    function obfusucate(email) {
+        var omail = '#';
+        var o2mail = email.split('@');
+        return(
 
+            <a onMouseEnter={() => {
+                document.querySelector('#obEmailLink').setAttribute(`href`,`mailto:${email}`);
+                document.querySelector('#obEmailLink').innerHTML = email
+            }} onMouseLeave={() => {
+                document.querySelector('#obEmailLink').setAttribute(`href`,`${omail}`);
+                document.querySelector('#obEmailLink').innerHTML = `<span class="obEmail">${o2mail[0]}</span><span>${o2mail[1]}</span>`
+            }} id="obEmailLink" href={`${omail}`}>
+                <span className="obEmail">{o2mail[0]}</span><span>{o2mail[1]}</span>
+            </a>
+
+        )
+    }
 
 return(
     <div className="about-container">
@@ -22,7 +38,7 @@ return(
             <h2>Hi, I'm June</h2>
             <p>Thank you for stopping by. I'm a Jr. frontend &amp; fullstack developer, musician, and dabbler from Durham, NC. I have a passion for design and art and bring that into my work.</p>
             <p>I have {getAge("2017/03/01")} years of experience working with HTML, CSS, Javascript, React, Liquid, Figma, and Adobe Creative Suites. I have {getAge("2020/03/01")} years of experience building and using APIs and backends with Express, Node, GraphQL, MySQL, and PostgreSQL.</p>
-            <p>I firmly believe in web development as an art form, and I'm always excited to speak to other creatives. If you're looking to connect shoot me an email at <a href="mailto:june@joonipea.com">june@joonipea.com</a></p>
+            <p>I firmly believe in web development as an art form, and I'm always excited to speak to other creatives. If you're looking to connect shoot me an email at {obfusucate("june@joonipea.com")}</p>
         </div>
     </div>
 )
